3a9ef006fd4b3b9f822f852a4235d40178602ba3
Victor Kozodaev <viktor7778123@gmail.com>
2021-12-03 20:05:31 +0300

    Configure Prettier; Add react-navigation and mobx deps; Remove ESLint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-};
+	bracketSpacing: true,
+	arrowParens: 'always',
+	quoteProps: 'consistent',
+	useTabs: true,
+	printWidth: 120,
+	tabWidth: 2,
+	singleQuote: true,
+	jsxBracketSameLine: true,
+	semi: false,
+	trailingComma: 'none'
+}
