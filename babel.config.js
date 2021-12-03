42e3fa4d8256c5f3860fda17a420e4a50616a26f
Victor Kozodaev <viktor7778123@gmail.com>
2021-12-03 20:50:25 +0300

    Configure: Typescript Alias, Decorators support, Svg Transformer

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-	presets: ['module:metro-react-native-babel-preset']
+	presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@lib': './src/lib',
+          '^@feature/(.+)': ([, name]) => `./src/features/${name}/shared`
+        }
+      }
+    ]
+  ]
 }
