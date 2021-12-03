802c992e337e25e08a3c4d30e1aaf9f323ddad2e
Victor Kozodaev <viktor7778123@gmail.com>
2021-12-03 22:51:28 +0300

    Feat: AuthScreen UI

--- src/features/user/screens/AuthScreen.tsx
+++ src/features/user/screens/AuthScreen.tsx
@@ -1,10 +1,38 @@
+import { Button, Icon, Layout, Text } from '@ui-kitten/components'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Logo } from '../components/Logo'
+import { StyleSheet } from 'react-native'
 
 export const AuthScreen = () => {
 	return (
-		<View>
-			<Text>AuthScreen</Text>
-		</View>
+		<Layout style={ss.container}>
+			<Logo />
+			<Text category="h1" style={ss.title}>
+				Для волонтеров
+			</Text>
+			<Text category="p1" style={ss.description}>
+				Приложение для коммуникации с волонтерами, еще какой-нибудь текст...
+			</Text>
+			<Button accessoryLeft={<Icon name="google" />} appearance="outline" size="giant">
+				Войти через Google
+			</Button>
+		</Layout>
 	)
 }
+
+const ss = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	title: {
+		marginTop: 5,
+		marginBottom: 10
+	},
+	description: {
+		textAlign: 'center',
+		marginBottom: 30
+	}
+})
